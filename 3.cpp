4b7684b3a8f72c4ba5d53bc23ca44598a698d6d9
oday-samara <102911987+oday-samara@users.noreply.github.com>
2022-04-25 21:30:50 +0300

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for ( int i=0; i<4; i++ ) {
+        for ( int j=0; j<4 ;j++ ) {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+/*
+0000
+1111
+2222
+3333
+*/
