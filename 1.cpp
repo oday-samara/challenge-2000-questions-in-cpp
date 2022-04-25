c1b96df2a366b59d4fdf0d79081278c09e74d6a1
oday-samara <102911987+oday-samara@users.noreply.github.com>
2022-04-25 21:28:05 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string name;
+    cout << "Enter your name : ";
+    cin >> name;
+
+
+    int age;
+    cout << endl << "Enter your age : ";
+    cin >> age;
+
+
+    cout << endl << "hello " << name << " your age " << age << endl  ;
+    return 0;
+}
